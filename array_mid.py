35eb1693ce9d215f8f1fceab3032d34cfe8fcd77
Fathima Zohara <fathimaakifa35@gmail.com>
2024-06-02 08:06:09 +0530

    maximum sum of subarray

--- array_mid.py
+++ array_mid.py
@@ -2,19 +2,59 @@
 
 arr = [1,2,3]
 
-# result = 0
-# for i in range(len(arr)):
-#     sum = 0
-#     for j in range(i, len(arr)):
-#         sum += arr[j]
-#         result = max(sum, result)
-# print(result)
+def maxSum(arr):
+    result = 0
+    for i in range(len(arr)):
+        sum = 0
+        for j in range(i, len(arr)):
+            sum += arr[j]
+            result = max(sum, result)
+    print(result)
 
 
 # Kadane's alg
 
-arr = [5,4,-1,7,8]
+arr = [-2,1,-3,4,-1,2,1,-5,4]
 
+def maxSumOfSubarray(arr):
+
+    maximum = float('-inf')
+    sum = 0
+
+    for i in range(len(arr)):
+        sum += arr[i]
+        if sum > maximum:
+            maximum = sum
+        if sum < 0:
+            sum = 0
+
+    return print(maximum)
+
+arr = [-2,-1,4,-2,5]
+
+def maxSumSubarray(arr):
+
+    start = end = 0
+    maximum = float('-inf')
+    sum = 0
+
+    for k in range(len(arr)):
+        sum += arr[k]
+        if sum > maximum:
+            maximum = sum
+            end = k
+        if sum < 0:
+            sum = 0
+            if k+1 <= len(arr)-1:
+                start = k+1
+            else:
+                start = k
+
+    print(arr[start:end+1])
+
+maxSumSubarray(arr)
+
+# maxSumOfSubarray(arr)
 
 
 
